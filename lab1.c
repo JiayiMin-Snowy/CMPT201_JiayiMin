f37d64abf00046e8bf1560d0cce9aeafe7052dfb
JiayiMin-Snowy <evanmin000@gmail.com>
2026-09-18 20:48:08 -0700

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,13 +1,30 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *input = NULL;
+size_t capacity = 0;
 int main(void){
-  string input;
+  char *chunks;
   printf("Enter some text will yah: ");
-  scanf("%d", &input);
-  
-  char *newptr= NULL;
-  char *chunks= strtok(line, " ", &newptr);
+  fflush(stdout);
+  ssize_t num_char = getline(&input, &capacity, stdin);
+  if (num_char == -1) {
+    printf("getline failed\n");
+    free(input);
+    return 0;
+  }
+
+  input[strcspn(input, "\n")] = '\0';
   printf("Tokens:\n");
-  while((chunks=strtok(Null, " ", &newptr))){
+  char *newptr= NULL;
+  chunks = strtok_r(input, " ", &newptr);
+
+
+  while (chunks != NULL) {
     printf("%s\n", chunks);
+    chunks = strtok_r(NULL, " ", &newptr);
   }
   free(input);
   return 0;
